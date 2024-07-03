ed1862d854afcf8c35cef673c3ba8c9e21af8fcb
Xuan Thang <thangijr@gmail.com>
2024-07-03 22:45:42 +0700

    update order bằng paypal

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 import authRouter from "./router/UserRouter";
 import ProductRouter from "./router/ProductRouter";
 import OrderRouter from "./router/OrderRouter";
+import PaymentRouter from "./router/PaymentRouter";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 const app = express();
@@ -15,7 +16,9 @@ app.use(bodyParser.json({ limit: "10mb" })); // Giới hạn cho JSON payload
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true })); // Giới hạn cho URL-encoded payload
 app.use(express.json());
 app.use(morgan("tiny"));
-
+//Lấy client id từ .env
+const CLIENT_ID = process.env.CLIENT_ID;
+console.log("CLIENT_ID", CLIENT_ID);
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -31,5 +34,6 @@ mongoose.connect("mongodb://localhost:27017/Shosy-Ecommerce-Starter");
 app.use("/api", authRouter);
 app.use("/api", ProductRouter);
 app.use("/api", OrderRouter);
+app.use("/api", PaymentRouter);
 
 export const viteNodeApp = app;
