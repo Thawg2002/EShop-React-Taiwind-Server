6c2f2086808f2b1272b15b5f486935ba579b10fe
Xuan Thang <thangijr@gmail.com>
2024-07-06 13:12:23 +0700

    update env với app.js

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use("/api", OrderRouter);
 app.use("/api", PaymentRouter);
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
 
